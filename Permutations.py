6a2cee35a3d01c150587977dc365913d172fedff
John <j24oh.uw@gmail.com>
2017-11-21 01:28:14 -0500

    one liner solution

--- Permutations.py
+++ Permutations.py
@@ -1,6 +1,3 @@
 def permutations(string):
-    #your code here
-    if len(string) == 1: return [string]
-    if len(set(string)) == 1: return [string]
-    for i in range(len(string)):
-        return [string[i] + e for i in range(len(string)) for e in permutations(string[:i]+string[i+1:])]
+    return [string] if len(set(string)) == 1 \
+                    else set([string[i] + e for i in range(len(string)) for e in permutations(string[:i] + string[i+1:])])
